f16fa5404bc3f55f3cdcea9fd069c1e896af1f8f
zhangyoung99 <zhangyoung99@github.com>
2017-10-12 11:11:58 +0800

    add signUp modalDialog

--- resume2/src/components/Toolbar.vue
+++ resume2/src/components/Toolbar.vue
@@ -2,6 +2,13 @@
   <div id="Toolbar">
     <div class="wrapper">
       <span class="logo">resumer</span>
+      <div class="actions">
+        <a class="button primary" href="#" @click.prevent="signUpDialogVisible = true">注册</a>
+        <myDialog title="注册" :visible="signUpDialogVisible" @close="signUpDialogVisible = false">
+          这是slot的内容
+        </myDialog>
+        <a class="button" href="#">登录</a>
+      </div>
     </div>
   </div>
 
@@ -9,23 +16,48 @@
 
 <script>
 
- export default {
-   name: 'Toolbar'
- }
+  import myDialog from './myDialog'
+  export default {
+   name: 'Toolbar',
+   data(){
+     return {
+       signUpDialogVisible: false
+     }
+   },
+   components:{ myDialog }
+  }
 
 </script>
 
-<style>
+<style lang="scss" scoped>
   #Toolbar {
     height: 64px;
     background: #fff;
-  }
-  #Toolbar>.wrapper {
-    font-size: 20px;
-    margin-left: 20px;
-  }
-  #Toolbar .wrapper .logo {
-    display: table;
-    line-height:64px;
+    .wrapper {
+      margin-left: 20px;
+      .logo {
+        font-size: 20px;
+        line-height: 64px;
+      }
+      .actions{
+        float: right;
+        margin-right: 40px;
+        line-height: 64px;
+      }
+    }
+    .button {
+      display: inline-block;
+      width: 72px;
+      // height: 24px;
+      line-height: 24px;
+      padding: 5px;
+      text-decoration: none;
+      border: 1px solid #e5e5e5;
+      text-align: center;
+    }
+    .primary {
+      background: #00C15E;
+      color: #fff;
+    }
   }
 </style>
---
+++ resume2/src/components/myDialog.vue
@@ -0,0 +1,59 @@
+<template>
+    <div class="wrapper" v-show="visible">
+        <div class="dialog">
+          <header>
+            <span>{{title}}</span>
+            <span class="close" @click="close">x</span>
+          </header>
+          <section>
+            <slot></slot>
+          </section>
+        </div>
+    </div>
+</template>
+
+<script>
+
+export default {
+    name: 'myDialog',
+    props: ['title','visible'],
+    methods: {
+        close() {
+            this.$emit('close')
+        }
+    }
+}
+
+</script>
+
+<style lang="scss" scoped>
+    .wrapper {
+        position: fixed;
+        top: 0;
+        left: 0;
+        right: 0;
+        bottom: 0;
+        margin: 0;
+        background: hsla(0, 0%,0%,0.25);
+        display: flex;
+        justify-content: center;
+        align-items: center;
+    }
+    .dialog {
+        background: #e5e5e5;
+        border: 1px solid #666;
+        min-width: 20rem;
+        min-height: 4rem;
+        > header {
+            height: 4rem;
+            padding: 1rem;
+            // line-height: 4rem;
+            display: flex;
+            justify-content: space-between;
+            border-bottom: 1px solid #666; 
+        }
+        > main {
+            padding: 1rem;
+        }
+    }
+</style>
